f446ffe92b14fe688a8d1383b9ffadbe1aad5965
JohanKYT <kevin.maquis@udla.edu.ec>
2025-05-26 18:15:36 -0500

    Se creo la base de datos y se creo el primer controlador con CRUD

---
+++ ProyectoReservaCanchasAPIsProgramacionIV/Controllers/AdministradorController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProyectoReservaCanchasAPIsProgramacionIV.Data;
+using ProyectoReservaCanchasAPIsProgramacionIV.Models;
+
+namespace ProyectoReservaCanchasAPIsProgramacionIV.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdministradorController : ControllerBase
+    {
+        private readonly BaseDatos_ReservarCanchas_ProyectoProgramacionIV _context;
+
+        public AdministradorController(BaseDatos_ReservarCanchas_ProyectoProgramacionIV context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Administrador
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Administrador>>> GetAdministradors()
+        {
+            return await _context.Administrador.ToListAsync();
+        }
+
+        // GET: api/Administrador/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Administrador>> GetAdministrador(int id)
+        {
+            var item = await _context.Administrador.FindAsync(id);
+            if (item == null)
+                return NotFound();
+
+            return item;
+        }
+
+        // POST: api/Administrador
+        [HttpPost]
+        public async Task<ActionResult<Administrador>> PostAdministrador(Administrador item)
+        {
+            _context.Administrador.Add(item);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetAdministrador), new { id = item.BannerId }, item);
+        }
+
+        // PUT: api/Administrador/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAdministrador(int id, Administrador item)
+        {
+            if (id != item.BannerId)
+                return BadRequest();
+
+            _context.Entry(item).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AdministradorExists(id))
+                    return NotFound();
+                else
+                    throw;
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/Administrador/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAdministrador(int id)
+        {
+            var item = await _context.Administrador.FindAsync(id);
+            if (item == null)
+                return NotFound();
+
+            _context.Administrador.Remove(item);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AdministradorExists(int id)
+        {
+            return _context.Administrador.Any(e => e.BannerId == id);
+        }
+    }
+}
---
+++ ProyectoReservaCanchasAPIsProgramacionIV/Data/BaseDatos_ReservarCanchas_ProyectoProgramacionIV.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoReservaCanchasAPIsProgramacionIV.Models;
+
+namespace ProyectoReservaCanchasAPIsProgramacionIV.Data
+{
+    public class BaseDatos_ReservarCanchas_ProyectoProgramacionIV : DbContext
+    {
+        public BaseDatos_ReservarCanchas_ProyectoProgramacionIV(DbContextOptions<BaseDatos_ReservarCanchas_ProyectoProgramacionIV> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Campus> Campus { get; set; } = default!;
+        public DbSet<ReservaImplemento> ReservaImplemento { get; set; } = default!;
+        public DbSet<Administrador> Administrador { get; set; } = default!;
+        public DbSet<Calendario> Calendario { get; set; } = default!;
+        public DbSet<Cancha> Cancha { get; set; } = default!;
+        public DbSet<Carrera> Carrera { get; set; } = default!;
+        public DbSet<Estudiante> Estudiante { get; set; } = default!;
+        public DbSet<Facultad> Facultad { get; set; } = default!;
+        public DbSet<Implemento> Implemento { get; set; } = default!;
+        public DbSet<PersonalMantenimiento> PersonalMantenimiento { get; set; } = default!;
+        public DbSet<Reserva> Reserva { get; set; } = default!;
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Discriminador para herencia de PersonaUdla
+            modelBuilder.Entity<PersonaUdla>()
+                .HasDiscriminator<string>("TipoPersona")
+                .HasValue<Estudiante>("Estudiante")
+                .HasValue<Administrador>("Administrador")
+                .HasValue<PersonalMantenimiento>("PersonalMantenimiento");
+
+            // Personalización de tabla para reservas
+            modelBuilder.Entity<Reserva>().ToTable("Reservas");
+
+        }
+    }
+}
